ecb47acf9df03fd9ced5cb076b65bbb4d289544f
27akim <criminal.ok1@gmail.com>
2021-03-02 14:56:11 +0300

    feat: add js code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array)
+{
+    let temp = 0;
+    if(array[0] != undefined)
+    {
+        temp = array[0];
+    }
+    else
+    {
+        return temp;
+    }
+    for (let i=0; i<array.length - 1; i++)
+    {
+        if(array[i] > array[i + 1])
+        {
+            temp = array[i + 1];
+        }
+    }
+    return temp;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array)
+{
+    let temp = 0;
+    if(array[0] != undefined)
+    {
+        temp = array[0];
+    }
+    else
+    {
+        return temp;
+    }
+    for (let i=0; i<array.length - 1; i++)
+    {
+        if(array[i] < array[i + 1])
+        {
+            temp = array[i + 1];
+        }
+    }
+    return temp;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array)
+{
+    let sum = 0;
+    if(array.length == 0)
+    {
+        return sum;
+    }
+    for (let i=0; i<array.length; i++)
+    {
+        sum += array[i];
+    }
+    return sum/array.length;
 }
